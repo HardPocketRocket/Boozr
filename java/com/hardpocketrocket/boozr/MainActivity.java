0f56351cfd430f4e29437b9b891916f75484b436
Robert <rober3392@gmail.com>
2018-11-15 22:25:23 +0100

    Designed an initial UI for the Main Screen

--- java/com/hardpocketrocket/boozr/MainActivity.java
+++ java/com/hardpocketrocket/boozr/MainActivity.java
@@ -2,12 +2,53 @@ package com.hardpocketrocket.boozr;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.FrameLayout;
 
 public class MainActivity extends AppCompatActivity {
+    CardView userInfoLayout;
+    CardView addDrinkLayout;
+    CardView graphViewLayout;
+    CardView twitterPostLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        userInfoLayout = findViewById(R.id.user_info_card);
+        addDrinkLayout = findViewById(R.id.add_drink_card);
+        graphViewLayout = findViewById(R.id.graph_card);
+        twitterPostLayout = findViewById(R.id.twitter_card);
+
+        userInfoLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        addDrinkLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        graphViewLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        twitterPostLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
---
+++ java/com/hardpocketrocket/boozr/User.java
@@ -0,0 +1,5 @@
+package com.hardpocketrocket.boozr;
+
+public class User {
+
+}
