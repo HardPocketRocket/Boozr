fcb5c40808170c333e7e555261d16720c0c2b393
Robert <rober3392@gmail.com>
2018-11-19 21:57:57 +0100

    A lot of UI additions and borders

--- java/com/hardpocketrocket/boozr/Drink.java
+++ java/com/hardpocketrocket/boozr/Drink.java
@@ -5,14 +5,10 @@ import io.realm.RealmObject;
 public class Drink extends RealmObject {
     private String name;
     private int cost;
-    private int amount;
-    private int alcoholContent;
 
     public Drink(String name, int cost, int amount, int alcoholContent) {
         this.name = name;
         this.cost = cost;
-        this.amount = amount;
-        this.alcoholContent = alcoholContent;
     }
 
     public Drink(){}
